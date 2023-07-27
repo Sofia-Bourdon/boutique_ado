46264797b1e6ca096ba73c96fbb6f93df1abdbc4
Sofia-Bourdon <>
2023-07-27 12:09:03 +0000

    added ad to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -8,7 +8,7 @@ def bag_contents(request):
     product_count = 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDART_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDART_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -19,7 +19,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count, 
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
